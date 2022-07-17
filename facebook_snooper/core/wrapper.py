b3fa2bc31ad1af055ec801e7b70a94a63f4c0814
Quurow <107263777+Quurow@users.noreply.github.com>
2022-07-17 21:58:40 +0300

    Delete facebook_snooper directory

--- facebook_snooper/core/wrapper.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod
-
-
-class BrowserWrapper:
-    def open(self, browser, url):
-        return browser.open(url)
-    
-    def submit_selected(self, browser):
-        return browser.submit_selected()
